31a385f239901a0b4e267a5dec7c04b2ac2a2c25
statios <statios@kakao.com>
2023-02-15 00:20:20 +0900

    fix: header layout

--- ChouxKream/Sources/Presentation/Template/Content/CKRCollectionController/Section/CKRSectionHeader.swift
+++ ChouxKream/Sources/Presentation/Template/Content/CKRCollectionController/Section/CKRSectionHeader.swift
@@ -30,28 +30,26 @@ class CKRSectionHeader: UICollectionReusableView {
         subtitleLabel.font = .systemFont(ofSize: 13, weight: .regular)
         subtitleLabel.textColor = .secondaryLabel
         
+        let vertical = UIStackView()
+        vertical.axis = .vertical
+        vertical.spacing = 4
+        vertical.addArrangedSubview(titleLabel)
+        vertical.addArrangedSubview(subtitleLabel)
+        
         deeplinkLabel = UILabel()
         deeplinkLabel.font = .systemFont(ofSize: 13, weight: .regular)
         deeplinkLabel.textColor = .secondaryLabel
         deeplinkLabel.setContentHuggingPriority(.required, for: .horizontal)
-        
-        let content = UIStackView()
-        content.axis = .vertical
-        content.spacing = 4
-        content.addArrangedSubview(titleLabel)
-        content.addArrangedSubview(subtitleLabel)
-        content.backgroundColor = .blue
         
         let horizontal = UIStackView()
         horizontal.axis = .horizontal
         horizontal.spacing = 4
         horizontal.isLayoutMarginsRelativeArrangement = true
         horizontal.layoutMargins = .init(top: 0, left: 0, bottom: 12, right: 0)
-        horizontal.translatesAutoresizingMaskIntoConstraints = false
-        horizontal.alignment = .lastBaseline
-        horizontal.addArrangedSubview(content)
+        horizontal.alignment = .bottom
+        horizontal.addArrangedSubview(vertical)
         horizontal.addArrangedSubview(deeplinkLabel)
-        horizontal.backgroundColor = .red
+        horizontal.translatesAutoresizingMaskIntoConstraints = false
         
         addSubview(horizontal)
         
@@ -71,7 +69,7 @@ class CKRSectionHeader: UICollectionReusableView {
         titleLabel.text = item.title
         subtitleLabel.isHidden = item.subtitle == nil
         subtitleLabel.text = item.subtitle
-        deeplinkLabel.isHidden = item.deeplink == nil
+//        deeplinkLabel.isHidden = item.deeplink == nil
         deeplinkLabel.text = "더보기"
     }
     
